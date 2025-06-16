3a233d9e39194a96dff0a87b585702e9796fa65f
samuu1i <elitete23153@gmail.com>
2025-06-16 16:11:08 -0300

    avisen si anda

---
+++ TDAPalabra.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "TDAPalabra.h"
+
+
+void palabraATitulo(Palabra* pal)
+{
+    char* iPal = pal->vPal;
+    *iPal = toupper(*iPal);
+    iPal++;
+
+    while(*iPal)
+    {
+        *iPal = tolower(*iPal);
+        iPal++;
+    }
+}
+
+void palabraAMinuscula(Palabra *pal)
+{
+    char *iPal = pal->vPal;
+
+    while(*iPal)
+    {
+        *iPal = tolower(*iPal);
+        iPal++;
+    }
+}
+
+
+void palabraMostrar(const Palabra* pal)
+{
+    puts(pal->vPal);
+}
+
+
+bool esLetra(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "TDAvector.h"
+#include "funciones.h"
+#define CAPACIDAD_DATOS 500
+#define TAM_LINEA 350
+#define NOMBRE_GENERAL "indices_icc_general_capitulos.csv"
+#define MES_MES 1
+#define ANIO_MES 12
+#define NOMBRE_BIN "arch.bin"
+int main()
+{
+    Vector items;
+    Vector general;
+    Vector unif;
+    vectorCrear(&items,CAPACIDAD_DATOS,sizeof(ITEMS));
+    vectorCrear(&general,CAPACIDAD_DATOS,sizeof(GENERAL));
+    vectorCrear(&unif,CAPACIDAD_DATOS,sizeof(UNIF));
+
+    archivoAVector("Indices_items_obra.csv",&items,leerLineaItems,TAM_LINEA);
+    archivoAVector(NOMBRE_GENERAL,&general,leerLineaGeneral,TAM_LINEA);
+
+    DesencriptarGeneral(&general);
+    DesencriptarItems(&items);
+    normalizar_general(&general);
+    formatoPeriodo(&items,eligeItems);
+    formatoPeriodo(&general,eligeGeneral);
+    normalizarItems(&items);
+    clasificadorGeneral(&general);
+    clasificadorItems(&items);
+    //mostrarVector(&items);
+   // mostrarVectorGeneral(&general);
+    unificar3(&unif,&items,&general);
+
+    //unificar(&items,&general);
+    //ordenarInsercion(&items,cmpClasificador);
+    //ordenarInsercion(&items,cmpPeriodo);
+
+    ordenarInsercion(&unif,cmpClasificador);
+    ordenarInsercion(&unif,cmpPeriodo);
+    varMensual(&unif);
+    varAnual(&unif);
+    unificarResult(&unif,NOMBRE_BIN);
+    //mostrarResultsBin(NOMBRE_BIN);
+   // mostrarVectorItes(&items);
+    //mostrarVectorUnif(&unif);
+
+    return 0;
+}
